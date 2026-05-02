719ae6755bc2b69b8c33049c8900b7032a9cdd59
rahuldey98 <r.dey711@gmail.com>
2026-05-02 21:17:31 +0530

    modify get attendance schema

--- src/attendance/get-attendance.ts
+++ src/attendance/get-attendance.ts
@@ -1,12 +1,10 @@
-import {z} from "zod";
-
-import {dateSchema} from "../common/enums";
-
-const attendanceQuerySchema = z.object({
-    classId: z.coerce.number().int().positive(),
-    date: dateSchema,
-});
+import { z } from "zod";
 
 export const getAttendanceSchema = z.object({
-    query: attendanceQuerySchema,
+    query: z.object({
+        fromDate: z.string().optional(),
+        toDate: z.string().optional(),
+    }),
 });
+
+export type GetAttendanceRequest = z.infer<typeof getAttendanceSchema>["query"];
