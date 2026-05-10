0ef6336c81ecc92737f88d3e443071ded70951fe
rahuldey98 <r.dey711@gmail.com>
2026-05-11 00:52:28 +0530

    release: v1.2.5

--- dist/entities/student.d.ts
+++ dist/entities/student.d.ts
@@ -1,7 +1,9 @@
 import { User } from './user';
 import { Course } from "./course";
 import { Class } from "./class";
-export interface Student extends User {
+export interface Student {
+    userId: number;
+    user: User;
     feesDate?: string;
     course?: Course;
     class?: Class;
