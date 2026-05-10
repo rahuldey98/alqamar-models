b026462cb1cb762039b04fa811dc1b830352e693
rahuldey98 <r.dey711@gmail.com>
2026-05-11 01:34:41 +0530

    refactor student and teacher model

--- src/entities/student.ts
+++ src/entities/student.ts
@@ -2,9 +2,7 @@ import { User } from './user';
 import {Course} from "./course";
 import {Class} from "./class";
 
-export interface Student {
-    userId: number,
-    user: User,
+export interface Student extends User{
     feesDate?: string;
     course?: Course;
     class?: Class;
